5440f0b2e61230ad1cf40c8c45f24459438f9dd9
alexalikiotis <alexalikiotis5@gmail.com>
2020-09-04 21:01:29 +0300

    bulding example using fastify

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const fastify = require('fastify');
+const fastifyStatic = require('fastify-static');
+
+const app = fastify({ logger: true });
+
+app.register(fastifyStatic, {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/',
+});
+
+app.get('/', (request, reply) => {
+  return reply.sendFile('index.html');
+});
+
+const schema = {
+  query: {
+    name: { type: 'string' },
+  },
+  response: {
+    200: {
+      type: 'object',
+      properties: {
+        content: { type: 'string' },
+      },
+    },
+  },
+};
+
+app.get('/api/greeting', { schema }, (request, reply) => {
+  const name = request.query ? request.query.name : undefined;
+  reply.send({ content: `Hello, ${name || 'World!'}` });
+});
+
+module.exports = app;
